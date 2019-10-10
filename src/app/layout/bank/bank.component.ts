eea49398539ef1d6e88ccc7ae318af33c61c8056
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-10-10 09:49:09 +0300

    #transenix.ver7  modify: bank component(s)

--- src/app/layout/bank/bank.component.ts
+++ src/app/layout/bank/bank.component.ts
@@ -5,6 +5,7 @@ import { ApiService } from '../../core/service/api.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { bankNew, bankToDto, createNewBank, dtoToBank } from '../../core/model/bank.model';
 import { first } from 'rxjs/operators';
+import { dtoToBankInfo } from '../../core/model/bank-info.model';
 
 @Component({
   selector: 'app-bank',
@@ -17,6 +18,9 @@ export class BankComponent implements OnInit {
   editForm: FormGroup;
   selectedBank;
   selectedBankId;
+  bankInfo;
+  selectedBankInfo;
+  selectedBankInfoId;
 
   constructor(private formBuilder: FormBuilder, private router: Router, private apiService: ApiService, public dataService: DataService) { }
 
@@ -32,18 +36,30 @@ export class BankComponent implements OnInit {
       mfo: [''],
       name: [''],
       phoneNumber: [''],
-      taxId: ['']
+      taxId: [''],
+      bankInfo: [''],
     });
 
     /**
      * PROD. Profile
      */
+    this.apiService.getBankInfo()
+      .subscribe( data => {
+          console.log(data)
+          this.bankInfo = dtoToBankInfo(data);
+        },
+        error => {
+          // alert( JSON.stringify(error) );
+          this.router.navigate(['login']); //TODO:  GET https://map1.mobo.cards:8093/api/v1/term-keys 401 ?
+        });
+
     this.apiService.findAllBanks()
       .subscribe( data => {
           console.log(data)
           for (let i = 0; i < data.content.length; i++) {
             const bank: any = data.content[i];
             var entity: any = dtoToBank(bank);
+            entity.bankInfo = this.bankInfo;
             this.banks.push(entity);
           }
         },
@@ -68,6 +84,20 @@ export class BankComponent implements OnInit {
       this.selectBank(bank);
     } else {
       this.selectedBankId = bank.id;
+    }
+  }
+
+  public selectBankInfo(bankInfo) {
+    console.log(bankInfo);
+    this.selectedBankInfo = Object.assign({}, bankInfo); // @see https://hassantariqblog.wordpress.com/2016/10/13/angular2-deep-copy-or-angular-copy-replacement-in-angular2
+    this.editForm.setValue(this.selectedBankInfo);
+  }
+
+  public selectBankInfoId(bankInfo) {
+    if (this.selectedBankInfoId === bankInfo.id) {
+      this.selectBankInfo(bankInfo);
+    } else {
+      this.selectedBankInfoId = bankInfo.id;
     }
   }
 
