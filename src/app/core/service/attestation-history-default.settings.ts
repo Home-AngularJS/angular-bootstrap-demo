d017ab94deffd20c012709e34f830dc23f8876c5
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-10-29 22:18:22 +0200

    #transenix-ver1.1.0  modify: footer component(s)

--- src/app/core/service/attestation-history-default.settings.ts
+++ src/app/core/service/attestation-history-default.settings.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class AttestationHistoryDefaultSettings {
   search = {};
 
-  slice = { page: 1, size: 19 };
+  slice = { page: 1, size: 18 };
 
   filter = {
     date: [{ operator: 'lt', type: 'date', value: '' }]
