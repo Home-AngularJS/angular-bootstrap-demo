1c99964bc312468a46ce5108be81aca48d7d063d
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-12-13 15:26:43 +0200

    #transenix-ver1.1.0  modify: statusAnalytics to analytics component(s)

--- src/app/layout/analytics/analytics.component.ts
+++ src/app/layout/analytics/analytics.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../core/service/data.service';
 import { Router } from '@angular/router';
 import { ApiService } from '../../core/service/api.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-analytics',
@@ -12,8 +13,96 @@ export class AnalyticsComponent implements OnInit {
   statusAnalytics = [];
   entryModeAnalytics = [];
   formFactorAnalytics = [];
+  startDate = new Date(0);
+  endDate = new Date(0);
 
-  constructor(private router: Router, private apiService: ApiService, public dataService: DataService) { }
+  multi = [
+    {
+      "name": "Germany",
+      "series": [
+        {
+          "name": "1990",
+          "value": 62000000
+        },
+        {
+          "name": "2010",
+          "value": 73000000
+        },
+        {
+          "name": "2011",
+          "value": 89400000
+        }
+      ]
+    },
+    {
+      "name": "USA",
+      "series": [
+        {
+          "name": "1990",
+          "value": 250000000
+        },
+        {
+          "name": "2010",
+          "value": 309000000
+        },
+        {
+          "name": "2011",
+          "value": 311000000
+        }
+      ]
+    },
+    {
+      "name": "France",
+      "series": [
+        {
+          "name": "1990",
+          "value": 58000000
+        },
+        {
+          "name": "2010",
+          "value": 50000020
+        },
+        {
+          "name": "2011",
+          "value": 58000000
+        }
+      ]
+    },
+    {
+      "name": "UK",
+      "series": [
+        {
+          "name": "1990",
+          "value": 57000000
+        },
+        {
+          "name": "2010",
+          "value": 62000000
+        }
+      ]
+    }
+  ];
+
+
+  view: any[] = [700, 300];
+
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
+  constructor(private router: Router, private toastr: ToastrService, private apiService: ApiService, public dataService: DataService) { }
 
   ngOnInit() {
     if (!window.localStorage.getItem('token')) {
@@ -31,7 +120,8 @@ export class AnalyticsComponent implements OnInit {
           this.viewTransactionsAnalytics(Object.assign({}, data.dailyAnalytics));
         },
         error => {
-          // alert( JSON.stringify(error) );
+          if (this.isNotEmpty(error.error)) this.showError('Аналитика', 'Error: ' + error.error.errorText + '; ErrorCode=' + error.error.errorCode + ';');
+          else this.showError('Аналитика', error.message);
         });
 
     /**
@@ -40,29 +130,47 @@ export class AnalyticsComponent implements OnInit {
   }
 
   viewTransactionsAnalytics(analytics) {
+    this.startDate = analytics.startDate;
+    this.endDate = analytics.endDate;
     this.statusAnalytics = [
       {
-        'name': 'Сумма',
+        'name': 'Успешная сумма',
         'series': [
           {
-            'name': 'Успешно',
-            'value': this.amountСonverter(analytics.statusAnalytics.successfulAmount)
-          },
-          {
-            'name': 'Отказ',
-            'value': this.amountСonverter(analytics.statusAnalytics.declinedAmount)
+            'name': '',
+            'value': this.amountСonverter(analytics.statusAnalytics.successfulAmount),
+            'extra': {
+              'code': 'de'
+            }
           }
         ]
       },
       {
-        'name': 'Количество',
+        'name': 'Отказная сумма',
         'series': [
           {
-            'name': 'Успешно',
-            'value': analytics.statusAnalytics.successfulCount
-          },
+            'name': '',
+            'value': this.amountСonverter(analytics.statusAnalytics.declinedAmount),
+            'extra': {
+              'code': 'de'
+            }
+          }
+        ]
+      },
+      {
+        'name': 'Количество успешных',
+        'series': [
           {
-            'name': 'Отказ',
+            'name': '',
+            'value': analytics.statusAnalytics.successfulCount
+          }
+        ]
+      },
+      {
+        'name': 'Количество отказов',
+        'series': [
+          {
+            'name': '',
             'value': analytics.statusAnalytics.declinedCount
           }
         ]
@@ -108,16 +216,44 @@ export class AnalyticsComponent implements OnInit {
       }];
   }
 
+  /**
+   * https://github.com/scttcper/ngx-toastr
+   * https://expertcodeblog.wordpress.com/2018/07/05/typescript-sleep-a-thread
+   */
+  showSuccess(title, message) {
+    this.toastr.success(message, title, {
+      timeOut: 2000
+    });
+  }
+
+  showError(title, message) {
+    this.toastr.error(message, title, {
+      timeOut: 20000
+    });
+  }
+
+  showWarning(title, message) {
+    this.toastr.warning(message, title, {
+      timeOut: 2000
+    });
+  }
+
+  showInfo(title, message) {
+    this.toastr.info(message, title, {
+      timeOut: 2000
+    });
+  }
+
   onSelectChart(data): void {
-    console.log("Item clicked", JSON.parse(JSON.stringify(data)));
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
   }
 
   onActivateChart(data): void {
-    console.log("Activate", JSON.parse(JSON.stringify(data)));
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
   }
 
   onDeactivateChart(data): void {
-    console.log("Deactivate", JSON.parse(JSON.stringify(data)));
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
   private amountСonverter(cents): number {
