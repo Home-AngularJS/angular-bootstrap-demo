2eeb570ecc4ef03718ee54abf668a88adfdc6335
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-12-23 23:02:26 +0200

    #transenix-ver1.1.0  modify: service-group component(s)

--- src/app/layout/service-group/service-group.component.spec.ts
+++ src/app/layout/service-group/service-group.component.spec.ts
@@ -1,25 +1,25 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { ServiceGroupComponent } from './service-group.component';
-//
-// describe('ServiceGroupComponent', () => {
-//   let component: ServiceGroupComponent;
-//   let fixture: ComponentFixture<ServiceGroupComponent>;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ServiceGroupComponent ]
-//     })
-//     .compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ServiceGroupComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServiceGroupComponent } from './service-group.component';
+
+describe('ServiceGroupComponent', () => {
+  let component: ServiceGroupComponent;
+  let fixture: ComponentFixture<ServiceGroupComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ServiceGroupComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ServiceGroupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
