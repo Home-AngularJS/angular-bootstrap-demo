ec6eb66b2a09e60afdc23406668ca7055e97036c
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2020-04-26 01:37:43 +0300

    #transenix-ver1.3.0  modify(2): style of form components to message module

--- src/app/core/service/merchant-message-default.settings.ts
+++ src/app/core/service/merchant-message-default.settings.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class MerchantMessageDefaultSettings {
   search = {};
 
-  slice = { page: 1, size: 17 };
+  slice = { page: 1, size: 15 };
 
   filter = {
     date: [{ operator: 'lt', type: 'id', value: '' }]
--- src/app/core/service/terminal-message-default.settings.ts
+++ src/app/core/service/terminal-message-default.settings.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class TerminalMessageDefaultSettings {
   search = {};
 
-  slice = { page: 1, size: 17 };
+  slice = { page: 1, size: 15 };
 
   filter = {
     date: [{ operator: 'lt', type: 'terminalId', value: '' }]
