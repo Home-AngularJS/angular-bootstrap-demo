19b5fe0da544a652ac703bdb73cbbc459d9e7b6f
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2020-04-26 02:33:26 +0300

    #transenix-ver1.3.0  modify(3): style of form components to message module

--- src/app/core/service/message-template-default.settings.ts
+++ src/app/core/service/message-template-default.settings.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class MessageTemplateDefaultSettings {
   search = {};
 
-  slice = { page: 1, size: 18 };
+  slice = { page: 1, size: 10 };
 
   filter = {
     date: [{ operator: 'lt', type: 'id', value: '' }]
